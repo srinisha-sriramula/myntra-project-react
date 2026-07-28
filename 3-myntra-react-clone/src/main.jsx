fb9305ca4217d8c8bd9aafc7a053df4809c5944d
srinisha-sriramula <srinishasriramula29@gmail.com>
2026-07-28 21:11:48 +0530

    m

--- 3-myntra-react-clone/src/main.jsx
+++ 3-myntra-react-clone/src/main.jsx
@@ -7,6 +7,7 @@ import Bag from "./routes/Bag.jsx";
 import Home from "./routes/Home.jsx";
 import { Provider } from "react-redux";
 import myntraStore from "./store/index.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const router = createBrowserRouter([
   {
--- 3-myntra-react-clone/src/routes/App.jsx
+++ 3-myntra-react-clone/src/routes/App.jsx
@@ -3,13 +3,21 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import HomeItem from "../components/HomeItem";
 import FetchItems from "../components/FetchItems";
+import { useSelector } from "react-redux";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 function App() {
+  const fetchStatus = useSelector((store) => store.fetchStatus);
   return (
     <>
       <Header></Header>
       <FetchItems></FetchItems>
-      <Outlet></Outlet>
+      {fetchStatus.currentlyFetching ? (
+        <LoadingSpinner></LoadingSpinner>
+      ) : (
+        <Outlet></Outlet>
+      )}
+
       <Footer></Footer>
     </>
   );
